5f4dcd16c50c466a7abf55799f00bfe42f68b0e5
Hector Caraballo <hcdisat@gmail.com>
2024-01-24 12:18:06 -0500

    - splash screen will be shown until the Router is fully ready to render the starting route

--- core/common/src/main/java/com/hcdisat/common/Constants.kt
+++ core/common/src/main/java/com/hcdisat/common/Constants.kt
@@ -3,4 +3,5 @@ package com.hcdisat.common
 object AppSettings {
     const val DATASTORE_NAME = "news_datastore"
     const val APP_ENTRY_KEY = "app_entry"
+    const val NEWS_API_KEY = "23726f19e6d54690ab6ccb5333c64813"
 }
